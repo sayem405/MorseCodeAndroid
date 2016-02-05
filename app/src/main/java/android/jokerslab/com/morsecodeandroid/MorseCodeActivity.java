258e6011ca6ca70c37c1a5d2e641ec306c3406bb
sayem405 <sayem.cse18@gmail.com>
2016-02-06 01:03:16 +0600

    morse code core added

--- app/src/main/java/android/jokerslab/com/morsecodeandroid/MorseCodeActivity.java
+++ app/src/main/java/android/jokerslab/com/morsecodeandroid/MorseCodeActivity.java
@@ -2,12 +2,18 @@ package android.jokerslab.com.morsecodeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MorseCodeActivity extends AppCompatActivity {
+
+    private static final String TAG = MorseCodeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morse_code);
+
+        String morseCode = new MorseCodeCore().getMorseCodeFromFile(null);
+        Log.d(TAG,morseCode);
     }
 }
---
+++ app/src/main/java/android/jokerslab/com/morsecodeandroid/MorseCodeCore.java
@@ -0,0 +1,13 @@
+package android.jokerslab.com.morsecodeandroid;
+
+import java.io.File;
+
+/**
+ * Created by sayem on 2/6/2016.
+ */
+public class MorseCodeCore {
+
+    public String getMorseCodeFromFile(File file) {
+        return "I am morse code";
+    }
+}
