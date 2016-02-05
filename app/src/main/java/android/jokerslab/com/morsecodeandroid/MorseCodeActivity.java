8ac19bae1c88c0757feed671502d25ce2207115c
sayem chowdhury <sayem chowdhury>
2016-02-06 00:31:09 +0600

    Initial commit

---
+++ app/src/main/java/android/jokerslab/com/morsecodeandroid/MorseCodeActivity.java
@@ -0,0 +1,13 @@
+package android.jokerslab.com.morsecodeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MorseCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morse_code);
+    }
+}
